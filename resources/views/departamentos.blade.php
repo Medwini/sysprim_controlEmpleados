742f9a482cee2a4de003a12d455343d9c584d7bb
Medwini <medwhidalgo@gmail.com>
2024-03-18 10:53:50 -0400

    estilos

--- resources/views/departamentos.blade.php
+++ resources/views/departamentos.blade.php
@@ -4,9 +4,8 @@
 
 @section('content')
 
-    <div class="contenedor">
-        <div class="cont_menup">
-            <nav class="cont_inicio navbar sticky-top navbar-light bg-light">
+        <div class="">
+            <nav class="">
                 <a href="/" class="btn btn-nav me-2 fw-bold">
                     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-home-up" width="50" height="50" viewBox="0 0 24 24" stroke-width="1.5" stroke="#02a0df" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
@@ -16,12 +15,11 @@
                         <path d="M22 19l-3 -3l-3 3" />
                     </svg>
                 </a>
-                <a href="/estados" class="btn btn-nav me-2 fw-bold">Estados Laborales</a>
-                <a href="/sexos" class="btn btn-nav me-2 fw-bold">Sexos</a>
-                <a href="/empleados" class="btn btn-nav me-2 fw-bold btn_inicio">Empleados</a>
+                <a href="/estados" class="btn me-2 fw-bold">Estados Laborales</a>
+                <a href="/sexos" class="btn me-2 fw-bold">Sexos</a>
+                <a href="/empleados" class="btn me-2 fw-bold btn_inicio">Empleados</a>
             </nav>
         </div>
 
         <livewire:departamentos />
-    </div>
 @endsection
